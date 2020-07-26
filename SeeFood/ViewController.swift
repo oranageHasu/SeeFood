ada85283a027a7923f4e963d32eae041cfff9757
oranageHasu <blair.petrachek@gmail.com>
2020-07-26 12:15:03 -0500

    Added basic ML solution for "SeeFood"
    This is a simple implementation that uses CoreML for local device Machine Learning to identify if a supplied image is a hotdog or not a hotdog.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera        // .photoLibrary (if we want photo library access)
+        imagePicker.allowsEditing = false
     }
 
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("ERROR - Loading CoreML Model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("ERROR - Model failed to process image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+}
+
+//MARK: - UIImagePicker Delegate Methods
+extension ViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            
+            guard let coreImage = CIImage(image: image) else {
+                fatalError("ERROR - Could not convert to CIImage.")
+            }
+            
+            detect(image: coreImage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
+//MARK: - UINavigation Delegate Methods
+extension ViewController: UINavigationControllerDelegate {
+    
+}
